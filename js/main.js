1ca01a37a1ed91f8560ced8e36ba94f834ab4d98
kamcha7ka <sledsib@ya.ru>
2020-12-03 01:09:25 +0300

    Добавит функцию генерации тестовых данных

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 import {mainPinButton, initiate, removeOverlay, addPin} from "./map.js";
+import {generatePoint} from "./mock.js";
 import {KeyCode} from "./util.js";
 import {CENTER_MAP_COORDS} from "./const.js";
+
+const OBJECTS_COUNT = 5;
 
 const mainPinButtonHandler = (evt) => {
   if (evt.type === `mousedown` || evt.keyCode === KeyCode.ENTER) {
     mainPinButton.remove();
     removeOverlay();
+
+    const points = new Array(OBJECTS_COUNT)
+      .fill(``)
+      .map((_point, index) => generatePoint(index));
 
     initiate(CENTER_MAP_COORDS);
     addPin(CENTER_MAP_COORDS);
--- js/util.js
+++ js/util.js
@@ -2,3 +2,13 @@ export const KeyCode = {
   ESC: 27,
   ENTER: 13
 };
+
+export const getRandomCount = (min, max) => {
+  return Math.ceil(Math.random() * (max - min) + min);
+};
+
+export const getRandomNumber = (min, max) => {
+  const lower = Math.min(min, max);
+  const upper = Math.max(min, max);
+  return lower + Math.random() * (upper - lower);
+};
